f6ba68433266c544ddfc760d2c1229b77abc9fb7
kissmrton9 <kissmrton9@gmail.com>
2021-01-31 22:50:00 +0100

    A working ver

--- src/app/page/cat01/cat01.component.ts
+++ src/app/page/cat01/cat01.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 //import { IProduct } from 'src/app/model/product';
 import { list } from 'src/app/service/product-service.service';
-import { listById } from '../../service/product-service.service';
+
 
 @Component({
   selector: 'app-cat01',
@@ -13,7 +13,7 @@ export class Cat01Component implements OnInit {
   //products: Product[] = this.productService.data;
 
   //constructor(private productService: ProductServiceService) { }
-  products = listById(1);
+  products = list;
   ngOnInit(): void {
   }
 
