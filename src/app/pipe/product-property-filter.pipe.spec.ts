7606a0bf2cf493e8ebaa615dd581b81b9a5f39bd
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2021-02-01 18:01:47 +0100

    add product property filter for search dropdowns

---
+++ src/app/pipe/product-property-filter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ProductPropertyFilterPipe } from './product-property-filter.pipe';
+
+describe('ProductPropertyFilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ProductPropertyFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
