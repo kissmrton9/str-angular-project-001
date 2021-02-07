9b3ec70592d1a9bd62f51b75dcd1ae303063f767
kissmrton9 <kissmrton9@gmail.com>
2021-02-07 16:36:20 +0100

    HttpClient related imports in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { Cat01Component } from './page/cat01/cat01.component';
 import { Cat02Component } from './page/cat02/cat02.component';
 import { Cat03Component } from './page/cat03/cat03.component';
 import { HomeComponent } from './page/home/home.component';
-//import { list } from 'src/app/service/product-service.service';
+import { HttpClientModule} from '@angular/common/http';
+import { ProductServiceService } from 'src/app/service/product-service.service';
 import { ProductCardComponent } from './common/product-card/product-card.component';
 import { ProductPagerComponent } from './common/product-pager/product-pager.component';
 import { ProductListComponent } from './common/product-list/product-list.component';
@@ -33,9 +34,10 @@ import { ProductPropertyFilterPipe } from './pipe/product-property-filter.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
