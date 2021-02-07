712d41595320d38b4eaccca09c7b0ebaa7410df0
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2021-02-07 22:57:12 +0100

    merge bugfix

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -15,15 +15,15 @@ export class HomeComponent implements OnChanges {
   constructor(private productService: ProductServiceService) { }
   productsObservable: Observable<IProduct[]> = this.productService.getAll();
   products: IProduct[];
-  featuredProducts$: IProduct[];
-  actionProducts$: IProduct[];
+  featuredProducts: IProduct[];
+  actionProducts: IProduct[];
   showProducts() {
     this.productsObservable
       .subscribe((data: IProduct[]) => {
         //console.log(data);
         this.products = data;
-        this.featuredProducts$ = data.filter(value => value.featured);
-        this.actionProducts$ = data.filter(value => value.discount);
+        this.featuredProducts = data.filter(value => value.featured);
+        this.actionProducts = data.filter(value => value.discount);
         //console.log(this.featuredProducts);
       });
     //console.log(this.products);
