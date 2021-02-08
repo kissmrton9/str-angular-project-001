157c9523ab6777c60babf2241605c4570b9df216
kissmrton9 <kissmrton9@gmail.com>
2021-02-08 17:06:21 +0100

    Fix category pages to work with asynchron data

--- src/app/page/cat02/cat02.component.ts
+++ src/app/page/cat02/cat02.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+//import { Input, OnChanges, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { IProduct } from 'src/app/model/product';
-import { list } from 'src/app/service/product-service.service';
+import { ProductServiceService } from '../../service/product-service.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cat02',
   templateUrl: './cat02.component.html',
   styleUrls: ['./cat02.component.scss']
 })
-export class Cat02Component implements OnInit {
+export class Cat02Component {
 
-  products: IProduct[] = list.filter(value => value.catId === 1);
-  featuredProducts: IProduct[] = list.filter(value => value.catId === 1 && value.featured);
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private productService: ProductServiceService){}
+  productsObservable: Observable<IProduct[]> = this.productService.getAll().pipe(
+    map(products => products.filter(product => product.catId === 1))
+  );
+  featuredProductsObservable: Observable<IProduct[]> = this.productsObservable.pipe(
+    map(products => products.filter(product => product.featured))
+  );
 }
