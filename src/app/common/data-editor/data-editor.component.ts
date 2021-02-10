8f90ecb6d1c3cfa09a0395b534706bb338c42092
kissmrton9 <kissmrton9@gmail.com>
2021-02-10 22:47:40 +0100

    Admin page list automatically updates itself

--- src/app/common/data-editor/data-editor.component.ts
+++ src/app/common/data-editor/data-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, ChangeDetectorRef } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IProduct, Product } from 'src/app/model/product';
 import { ITableCol } from 'src/app/service/config.service';
@@ -13,7 +13,7 @@ import { Sorter } from 'src/app/model/sorter';
   styleUrls: ['./data-editor.component.scss']
 })
 
-export class DataEditorComponent {
+export class DataEditorComponent{
 
   @Input() products: IProduct[] = [];
   @Input() filter: Filter;
@@ -33,22 +33,59 @@ export class DataEditorComponent {
   constructor(
     private productService: ProductServiceService,
     private config: ConfigService,
-  ) { }
+//    private ref: ChangeDetectorRef
+  ){}
+  
+  trackItem (product: IProduct) {
+    return product.id;
+  }
+  
+  // ngOnChanges(){
+  //   if(this.products && this.products.length >= 50){
+  //     this.ref.detach();
+  //   }
+  // }
+
+  maxId(): number{
+    return Math.max(...this.products.map(product => product.id));
+  }
 
   onCreate(product: Product): void {
+    const baseid = product.id;
+    const newid = this.maxId() + 1;
+    const num = this.products.indexOf(product);
     this.productService.add(product).subscribe(
-      addedProduct => console.log(addedProduct)
+      () => console.log('Created product ' + newid + 'based on' + baseid)
     );
+    // Changing this.products to make angular *ngFor refresh the list
+    product.id = newid; //give new id
+    this.productService.getOne(baseid).subscribe(
+      (p) => {
+        this.products[num] = p;
+        this.products.splice(num,0,product);
+      }
+    );
+//    this.ref.detectChanges();
   }
   onUpdate(product: Product): void {
     this.productService.update(product).subscribe(
-      updatedProduct => console.log(updatedProduct)
+      updatedProduct => console.log('Updated product:' + updatedProduct.id)
     );
   }
   onDelete(product: Product): void {
+    const id = product.id;
     this.productService.remove(product).subscribe(
-      () => console.log('deleted')
+      () => console.log('Deleting product:' + product.id)
     );
+    // This doesn't work, angular doesn't refresh:
+    //
+    this.products.splice(this.products.indexOf(product),1);
+//    this.ref.detectChanges();
+    //
+    // But this works:
+    //let list: IProduct[] = this.products;
+    //list.splice(id,1);
+    //this.products = list;
   }
 
 }
